d5e613f402bf0738d20a0cb2aa721c1c185fcc40
Rimida Panzer <rimida@futurefoundry.co>
2017-09-11 01:14:53 -0400

    implemented destroy for portfolio items

--- app/controllers/portfolios_controller.rb
+++ app/controllers/portfolios_controller.rb
@@ -36,7 +36,13 @@ class PortfoliosController < ApplicationController
   end
 
   def destroy
+    # Perform the lookup
+    @portfolio_item = Portfolio.find(params[:id])
+
+    # Destroy/delete the record
     @portfolio_item.destroy
+
+    #Redirect
     respond_to do |format|
       format.html { redirect_to portfolios_path, notice: 'Portfolio item removed.' }
     end
