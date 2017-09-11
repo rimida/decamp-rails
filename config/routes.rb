1c7521d2ecd42179786d6dd9b13694de0760701d
Rimida Panzer <rimida@futurefoundry.co>
2017-09-11 01:47:42 -0400

    Customized portfolio show route

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,7 @@
 Rails.application.routes.draw do
-  resources :portfolios
-
+  resources :portfolios, except: [:show]
+  get 'portfolio/:id', to: 'portfolios#show', as:'portfolio_show'
+  
   resources :blogs
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 
